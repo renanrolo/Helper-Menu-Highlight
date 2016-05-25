d5f0f072f97dcbd846c6e51099b13ae109dfc5bd
Renan Rolo <renanrolo@hotmail.com>
2016-05-25 15:27:00 -0400

    Helper para inserir classe ATIVA funcionando

---
+++ HelperMenuHighlight/Helper/HelperMenuHighlight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helper
+{
+    public static class HelperMenuHighlight
+    {
+        public static string ObterController()
+        {
+            var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+
+            if (routeValues.ContainsKey("controller"))
+                return (string)routeValues["controller"];
+
+            return string.Empty;
+        }
+
+        public static string ObterAction()
+        {
+            var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+
+            if (routeValues.ContainsKey("action"))
+                return (string)routeValues["action"];
+
+            return string.Empty;
+        }
+
+        public static MvcHtmlString LinkMenuHighlight(this HtmlHelper helper, string controller, string action, string classCSS = "default")
+        {
+            var link = new TagBuilder("a");
+
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext, helper.RouteCollection);
+
+            link.MergeAttribute("href", urlHelper.Action(action, controller));
+
+            string requestAction = ObterAction();
+            string requestController = ObterController();
+
+            if (controller.Equals(requestController) && action.Equals(requestAction))
+            {
+                link.MergeAttribute("class", classCSS);
+            }
+
+            return new MvcHtmlString(link.ToString());
+        }
+    }
+}
---
+++ HelperMenuHighlight/HelperMenuHighlight/Models/HelperMenuHighlight.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMenuHighlight.Models
+{
+    public static class HelperMenuHighlight
+    {
+        public static string ObterController()
+        {
+            var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+
+            if (routeValues.ContainsKey("controller"))
+                return (string)routeValues["controller"];
+
+            return string.Empty;
+        }
+
+        public static string ObterAction()
+        {
+            var routeValues = HttpContext.Current.Request.RequestContext.RouteData.Values;
+
+            if (routeValues.ContainsKey("action"))
+                return (string)routeValues["action"];
+
+            return string.Empty;
+        }
+
+        public static MvcHtmlString LinkMenuHighlight(this HtmlHelper helper, string controller, string action, string classCSS = "active")
+        {
+            var link = new TagBuilder("a");
+
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext, helper.RouteCollection);
+
+            link.MergeAttribute("href", urlHelper.Action(action, controller));
+
+            string requestAction = ObterAction();
+            string requestController = ObterController();
+
+            if (controller.Equals(requestController) && action.Equals(requestAction))
+            {
+                link.MergeAttribute("class", classCSS);
+            }
+
+            return new MvcHtmlString(link.ToString());
+        }
+
+        public static string ClasseHighlight(this HtmlHelper helper, string action, string controller, string classCSS = "active")
+        {
+            string requestAction = ObterAction();
+            string requestController = ObterController();
+
+            if (controller.Equals(requestController) && action.Equals(requestAction))
+                return classCSS;
+
+            return String.Empty;
+        }
+    }
+}
